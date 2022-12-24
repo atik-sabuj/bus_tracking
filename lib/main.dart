7977e6af416425823d0720a74cd1415d8f94c8ba
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-12-24 15:06:04 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'pages/login_sample_from_mentor.dart';
+import 'pages/login_ui_at_lesson.dart';
+import 'pages/sign_up_2.dart';
+import 'pages/sign_up_assignment.dart';
+
+
+void main()=> runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Sample1(),
+      routes: {
+        Sample1.id: (context) => const Sample1(),
+        Lesson.id: (context) => const Lesson(),
+        Assignment.id: (context) => const Assignment(),
+        SignUpPage.id: (context) => const SignUpPage(),
+      },
+    );
+  }
+}
